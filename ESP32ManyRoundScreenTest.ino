2941ac71274bbbbaac2f26e496d79960ca649b2c
charles <cgiguere@iname.com>
2023-08-18 11:22:12 -0400

    modified Animated GIF library

--- ESP32ManyRoundScreenTest.ino
+++ ESP32ManyRoundScreenTest.ino
@@ -1,33 +1,38 @@
-// Animated GIF with Multiple Displays
+// Animated GIF with Round Display
 //
 // ESP32 40MHz SPI single frame rendering performance
+// Note: no DMA performance gain on smaller images or transparent pixel GIFs
+//
+#define USE_DMA      // ESP32 ~1.25x single frame rendering performance boost for badgers.h
+                     // Note: Do not use SPI DMA if reading GIF images from SPI SD card on same bus as TFT
+#define NORMAL_SPEED // Comment out for rame rate for render speed test
 
 #include <SPI.h>
-#include <TFT_eSPI.h>    // Install this library with the Arduino IDE Library Manager
-#include <AnimatedGIF.h> // Install this library with the Arduino IDE Library Manager
+#include <TFT_eSPI.h>
+#include "AnimatedGIF.h"
+
+// Examples images
+#include "images/hyperspace.h"
+#include "images/nostromo.h"
+#include "images/hud_1.h"
+#include "images/hud_2.h"
+#include "images/hud_5.h"
+#include "images/hud_6.h"
+#include "images/hud_7.h"
 #include "images/darthvader.h"
 #include "images/x_wing.h"
-#include "images/hud_1.h"
-#include "images/hud_6.h"
-#include "images/hud_2.h"
 
-const int NUM_DISPLAYS = 4; // Adjust this value based on the number of displays
-AnimatedGIF gifs[NUM_DISPLAYS];
+const int NUM_DISPLAYS = 6;                     // Adjust this value based on the number of displays
+const int CS_PINS[NUM_DISPLAYS] = {19, 22, 21,32,33,25}; // Add more pins if you have more displays
 
-// Define CS pins for all displays
-const int CS_PINS[NUM_DISPLAYS] = {19, 22, 21, 32}; // Add more pins if you have more displays
-// const int CS_PINS[NUM_DISPLAYS] = {19, 22, 21, 32, 33}; // Add more pins if you have more displays
+AnimatedGIF gif_1;
+AnimatedGIF gif_2;
+AnimatedGIF gif_3;
+AnimatedGIF gif_4;
+AnimatedGIF gif_5;
+AnimatedGIF gif_6;
 
-// Define the images for all displays
-const uint8_t *GIF_IMAGES[NUM_DISPLAYS] = {x_wing, darthvader, hud_6, hud_1};                                      // Add more images if needed
-const size_t GIF_SIZES[NUM_DISPLAYS] = {sizeof(x_wing), sizeof(darthvader), sizeof(hud_6), sizeof(hud_1)}; // Add more sizes if needed
-
-//Free Heap with 3 Animated GIF : 241248
-//Free Heap with 4 Animated GIF : 217332
-// ld.exe: region `dram0_0_seg' overflowed by 17664 bytes
-// ld.exe: region `dram0_0_seg' overflowed by 17264 bytes
-
-TFT_eSPI tft;
+TFT_eSPI tft = TFT_eSPI();
 
 void setup()
 {
@@ -37,44 +42,49 @@ void setup()
   for (int i = 0; i < NUM_DISPLAYS; i++)
   {
     pinMode(CS_PINS[i], OUTPUT);
-    digitalWrite(CS_PINS[i], HIGH); // Deselect the display
-  }
-
-  for (int i = 0; i < NUM_DISPLAYS; i++)
-  {
-    digitalWrite(CS_PINS[i], LOW); // Select the display
+    digitalWrite(CS_PINS[i], LOW); // select the display
     tft.fillScreen(TFT_BLACK);
-    tft.setRotation(2); // Adjust Rotation of your screen (0-3)
-    gifs[i].begin(BIG_ENDIAN_PIXELS);
-    if (!gifs[i].open((uint8_t *)GIF_IMAGES[i], GIF_SIZES[i], GIFDraw))
-    {
-      Serial.printf("Failed to open GIF%d\n", i + 1);
-      Serial.printf("Last Error=%d", gifs[i].getLastError());
-      Serial.printf("Image size=%d", sizeof(GIF_IMAGES[i]));
-    }
+    tft.setRotation(2);             // Adjust Rotation of your screen (0-3)
     digitalWrite(CS_PINS[i], HIGH); // Deselect the display
   }
-  Serial.printf("Free Heap = %ld",ESP.getFreeHeap());
-}
 
+  openGif(&gif_1, hyperspace, sizeof(hyperspace));
+  openGif(&gif_2, hud_6, sizeof(hud_6));
+  openGif(&gif_3, nostromo, sizeof(nostromo));
+  openGif(&gif_4, x_wing, sizeof(x_wing));
+  openGif(&gif_5, hud_2, sizeof(hud_2));
+  openGif(&gif_6, hud_7, sizeof(hud_7));
+}
 void loop()
 {
-  for (int i = 0; i < NUM_DISPLAYS; i++)
+  playGif(&gif_1, 0);
+  playGif(&gif_2, 1);
+  playGif(&gif_3, 2);
+  playGif(&gif_4, 3);
+  playGif(&gif_5, 4);
+  playGif(&gif_6, 5);
+}
+
+void openGif(AnimatedGIF *gif, const uint8_t *gifImage, int gifSize)
+{
+  gif->begin(BIG_ENDIAN_PIXELS);
+  if (!gif->open((uint8_t *)gifImage, gifSize, GIFDraw))
   {
-    digitalWrite(CS_PINS[i], LOW); // Select the display
-
-    tft.startWrite();
-
-    int res = gifs[i].playFrame(false, NULL);
-    if (res == 0)
-    {
-      // If no more frames are available, reset the GIF to the beginning
-      gifs[i].reset();
-      gifs[i].playFrame(false, NULL);
-    }
-
-    tft.endWrite();
-
-    digitalWrite(CS_PINS[i], HIGH); // Deselect the display
+    Serial.printf("Could not open gif \n");
   }
+}
+
+void playGif(AnimatedGIF *gif, int screenIndex)
+{
+  digitalWrite(CS_PINS[screenIndex], LOW); // Select the display
+  tft.startWrite();
+  int res = gif->playFrame(false, NULL);
+  if (res == 0)
+  {
+    // If no more frames are available, reset the GIF to the beginning
+    gif->reset();
+    gif->playFrame(false, NULL);
+  }
+  tft.endWrite();
+  digitalWrite(CS_PINS[screenIndex], HIGH); // Deselect the display
 }
